bed8c07b77a952ecdabf68aefff49c00fcc54249
Alisa <alisamun2006@gmail.com>
2023-12-30 21:28:44 +1000

    hm

---
+++ 5. Templates and functors/2/2/2.cpp
@@ -0,0 +1,63 @@
+﻿
+#include <iostream>
+
+template <class T> 
+class table {
+public:
+    int rows, columns;
+    T **array;
+    table(int rows, int columns) : rows(rows), columns(columns) {
+        array = new T* [rows];
+        for (int i = 0; i < rows; ++i) {
+            array[i] = new T [columns];
+        }
+    }
+    T* operator[](int s) {
+        return array[s];
+    }
+    T* operator[](int s) const {
+        return array[s];
+    }
+    
+    T& operator=(const T& other) {
+        if (this != &other) {
+            delete[] array;
+            rows = other.rows;
+            columns = other.columns;
+            auto array = table<int>(rows, columns);
+            for (int i = 0; i < rows; ++i) {
+                array[i] = new T[columns];
+            }
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    array[i][j] = other.array[i][j];
+                }
+            }
+        }
+        return *this;
+    }
+    
+};
+
+
+int main()
+{
+    auto test = table<int>(2, 3);
+    test[1][2];
+    test[0][0] = 4;
+
+    std::cout << test[0][0]; 
+    auto test1 = table<std::string>(2, 3);
+    test1[0][0] = "Hello";
+
+    const table<int> tt(2, 3);
+    tt[0][0] = 4;
+    std::cout << tt[0][0];
+
+   
+    table<int> t1(2, 3);
+    table<int> t2(2, 3);
+
+    t1 = t2;
+   
+}
---
+++ 5. Templates and functors/3/3/3.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+class functor {
+public:
+    int get_sum = []() {
+        int sum = 0;
+        for (auto i = this.begin(); i < this.end(); ++i) {
+            if (i % 3 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    };
+    int get_count = []() {
+        int count = 0;
+        for (auto i = this.begin(); i < this.end(); ++i) {
+            if (i % 3 == 0) {
+                count += 1;
+            }
+        }
+        return count;
+        };
+};
+
+int main()
+{
+    std::vector<int> vector = { 4, 1, 3, 6, 25, 54 };
+    functor f;
+
+    f = std::for_each(vector.begin(), vector.end(), f);
+
+    std::cout << "[OUT] : get_sum() = " << f.get_sum() << std::endl;
+    std::cout << "[OUT] : get_count() = " << f.get_count() << std::endl;
+}
